2ef9c363a4bc9baa737b84af5bda7b1ef52cf695
Hector Salazar <hecsalazarf@outlook.com>
2021-02-06 14:15:09 -0600

    Allow to return raw pointer from read-write transaction

--- src/transaction.rs
+++ src/transaction.rs
@@ -272,6 +272,14 @@ impl<'env> RwTransaction<'env> {
         }
     }
 
+    /// Returns a raw pointer to the underlying LMDB read-write transaction.
+    ///
+    /// The caller must ensure that the pointer is not dereferenced after the
+    /// lifetime of the environment.
+    pub fn rw_txn(&self) -> *mut ffi::MDB_txn {
+        self.txn
+    }
+
     /// Opens a database in the provided transaction, creating it if necessary.
     ///
     /// If `name` is `None`, then the default database will be opened, otherwise
